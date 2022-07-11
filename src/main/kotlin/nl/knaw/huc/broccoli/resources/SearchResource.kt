7a3b975df009ce6e56076e556c6340a880ff0dec
HDJ <hayco@users.noreply.github.com>
2022-07-11 14:42:23 +0200

    WIP: yield start->end lines of text from mocked sample

--- src/main/kotlin/nl/knaw/huc/broccoli/resources/SearchResource.kt
+++ src/main/kotlin/nl/knaw/huc/broccoli/resources/SearchResource.kt
@@ -26,26 +26,63 @@ class SearchResource {
         return Response.ok(buildResult(canvasId)).build()
     }
 
-    private fun buildResult(canvasId: String) = AnnoTextResult(
-        id = canvasId,
-        anno = loadMockAnno(),
-        text = listOf(
-            "line1",
-            "line2",
-            "line3",
-            "line4",
-        )
-    )
+    private fun buildResult(canvasId: String): AnnoTextResult {
+        val anno = loadMockAnno()
 
-    @Suppress("UNCHECKED_CAST")
+        @Suppress("UNCHECKED_CAST")
+        val text = getMockedText(anno as List<Map<String, Any>>)
+
+        return AnnoTextResult(
+            id = canvasId,
+            anno = anno,
+            text = text
+        )
+    }
+
+    private fun getMockedText(annos: List<Map<String, Any>>): List<String> {
+        val anno = annos[0]
+        val id = anno["id"]
+        log.info("anno[0].id = $id")
+
+        val scanpage = annos.find { getBodyValue(it) == "scanpage" }.orEmpty()
+        log.info("scanpage: $scanpage")
+
+        val target = scanpage["target"] as Array<*>
+        val textTarget = target.find { (it as HashMap<*, *>)["type"] == "Text" } as HashMap<*, *>
+        val textSelector = textTarget["selector"] as HashMap<*, *>
+        val start = textSelector["start"]
+        val end = textSelector["end"]
+        log.info("start=$start, end=$end")
+
+        val mockedText = ArrayList<String>()
+
+        val reader = ResourceLoader.asReader("mock/text-lines.json")
+        val json = JSON.parse(reader)
+        log.info("json: $json")
+        if (json is Array<*>) {
+            for (line in json) {
+                if (line is String) {
+                    mockedText.add(line)
+                }
+            }
+        }
+        return mockedText
+    }
+
+    private fun getBodyValue(anno: Map<String, Any>): String? {
+        val body = anno["body"]
+        return if (body is HashMap<*, *>) body["value"] as String else null
+    }
+
     private fun loadMockAnno(): List<Any> {
-        var mockedAnnos = ArrayList<Any>()
+        val mockedAnnos = ArrayList<Any>()
         for (i in 0..3) {
             val reader = ResourceLoader.asReader("mock/anno-page-$i.json")
             val annoPage = JSON.parse(reader)
             if (annoPage is HashMap<*, *>) {
                 val items = annoPage["items"]
                 if (items is Array<*>) {
+                    @Suppress("UNCHECKED_CAST")
                     mockedAnnos.addAll(items as Array<Any>)
                 } else {
                     log.info("AnnotationPage[\"items\"] not a list, skipping: $items.")
