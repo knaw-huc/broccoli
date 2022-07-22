ab049bc9fc0bec159e218b57af718facc14f8fc5
HDJ <hayco@users.noreply.github.com>
2022-07-22 11:20:24 +0200

    Return runtime exceptions in JSON

--- src/main/kotlin/nl/knaw/huc/broccoli/resources/RuntimeExceptionMapper.kt
+++ src/main/kotlin/nl/knaw/huc/broccoli/resources/RuntimeExceptionMapper.kt
@@ -1,15 +1,17 @@
 package nl.knaw.huc.broccoli.resources
 
+import io.dropwizard.jersey.errors.ErrorMessage
 import javax.ws.rs.core.MediaType
 import javax.ws.rs.core.Response
+import javax.ws.rs.core.Response.Status.BAD_REQUEST
 import javax.ws.rs.ext.ExceptionMapper
 import javax.ws.rs.ext.Provider
 
 @Provider
 class RuntimeExceptionMapper : ExceptionMapper<RuntimeException> {
     override fun toResponse(exception: RuntimeException): Response =
-        Response.status(Response.Status.BAD_REQUEST)
-            .entity(exception.message)
-            .type(MediaType.TEXT_PLAIN)
+        Response.status(BAD_REQUEST)
+            .entity(ErrorMessage(BAD_REQUEST.statusCode, exception.message))
+            .type(MediaType.APPLICATION_JSON)
             .build()
 }
