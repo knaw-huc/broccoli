bfb6d9b69b040161ffb3a8cc0ba26213448c5df6
HDJ <hayco@users.noreply.github.com>
2022-08-03 16:24:50 +0200

    WIP work out how to get all the parts from various server calls

--- src/main/kotlin/nl/knaw/huc/broccoli/service/anno/AnnoFetcher.kt
+++ src/main/kotlin/nl/knaw/huc/broccoli/service/anno/AnnoFetcher.kt
@@ -1,11 +1,14 @@
 package nl.knaw.huc.broccoli.service.anno
 
+import com.jayway.jsonpath.Configuration.defaultConfiguration
 import com.jayway.jsonpath.JsonPath
+import com.jayway.jsonpath.Option.DEFAULT_PATH_LEAF_TO_NULL
 import nl.knaw.huc.broccoli.config.RepublicConfiguration
 import nl.knaw.huc.broccoli.config.RepublicVolume
 import org.slf4j.LoggerFactory
 import javax.ws.rs.client.ClientBuilder
-import javax.ws.rs.client.Entity
+import javax.ws.rs.client.Entity.json
+import javax.ws.rs.core.GenericType
 
 class AnnoFetcher(
     private val annoRepoURI: String, private val conf: RepublicConfiguration
@@ -16,7 +19,13 @@ class AnnoFetcher(
     private val client = ClientBuilder.newClient()
 
     override fun getScanAnno(volume: RepublicVolume, opening: Int): Map<String, Any> {
-        val webTarget = client.target(annoRepoURI).path("search").path("volume-${volume.name}").path("annotations")
+        val jsonParser = JsonPath.using(defaultConfiguration().addOptions(DEFAULT_PATH_LEAF_TO_NULL))
+
+        val volumeName = "volume-${volume.name}"
+        val webTarget = client.target(annoRepoURI)
+            .path("search")
+            .path(volumeName)
+            .path("annotations")
         log.info("path: ${webTarget.uri}")
 
         val archNr = conf.archiefNr
@@ -25,30 +34,56 @@ class AnnoFetcher(
         val bodyId = "urn:republic:NL-HaNA_${archNr}_${invNr}_${scanNr}"
         log.info("constructed bodyId: $bodyId")
 
-        val response = webTarget.request().post(
-            Entity.json(
-                mapOf("body.id" to bodyId)
-            )
-        )
+        val response = webTarget.request().post(json(mapOf("body.id" to bodyId)))
         log.info("code: ${response.status}")
 
-//        val body = response.readEntity(Map::class.java) as Map<String, Any>
         val body = response.readEntity(String::class.java)
-        val json = JsonPath.parse(body)
+        val json = jsonParser.parse(body)
         val data = json.read<List<Map<String, *>>>("$.items[0].target")
         log.info("data: $data")
+        val text = ArrayList<String>()
+        val annos = ArrayList<Map<String, Any>>()
         data.filter { it["type"] == "Text" }
             .forEach {
                 if (it["selector"] != null) {
                     @Suppress("UNCHECKED_CAST")
                     val selector = it["selector"] as Map<String, Any>
-                    val start = selector["start"]
-                    val end = selector["end"]
-                    log.info("Here be selector: $start..$end")
+                    val start = selector["start"] as Int
+                    val end = selector["end"] as Int
+
+                    // initial request
+                    var request = client.target(annoRepoURI)
+                        .path("search")
+                        .path(volumeName)
+                        .path("overlapping_with_range")
+                        .queryParam("target.source", it["source"] as String)
+                        .queryParam("range.start", start)
+                        .queryParam("range.end", end)
+                    log.info("Initial anno URI: ${request.uri}")
+
+                    while (annos.count() < 1000) { // some arbitrary cap for now
+                        val resp = request.request().get()
+                        val annoBody = resp.readEntity(String::class.java)
+                        val annoJson = jsonParser.parse(annoBody)
+                        annos.addAll(
+                            // Thanks https://regexland.com/regex-match-all-except/
+                            // Can be removed when AnnoRepo supports this as part of the query
+                            annoJson.read<List<Map<String, Any>>>("$.items[?(@.body.type =~ /^(?!.*(Line?|Page?|TextRegion?|Scan?)).*/)]")
+                        )
+                        request = client.target(annoJson.read<String>("$.next") ?: break)
+                    }
                 } else {
-                    log.info("Here be text: ${it["source"]}")
+                    val resp = client.target(it["source"] as String).request().get()
+                    text.addAll(resp.readEntity(object : GenericType<List<String>>() {}))
                 }
             }
-        return mapOf("foo" to "bar")
+        for ((i, anno) in annos.withIndex()) {
+            @Suppress("UNCHECKED_CAST") val b = anno["body"] as Map<String, Any>
+            log.info("anno[$i].body.id = ${b["id"]}")
+        }
+        return mapOf(
+            "anno" to annos,
+            "text" to text
+        )
     }
 }
