214f27099b25324efb71131bed93627ded8ee607
HDJ <hayco@users.noreply.github.com>
2022-09-20 13:39:58 +0200

    Out with v0, deprecate v1, v2 is hot (for now)

--- src/main/kotlin/nl/knaw/huc/broccoli/resources/RepublicResource.kt
+++ src/main/kotlin/nl/knaw/huc/broccoli/resources/RepublicResource.kt
@@ -1,6 +1,5 @@
 package nl.knaw.huc.broccoli.resources
 
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import io.swagger.v3.oas.annotations.Operation
 import nl.knaw.huc.broccoli.api.*
 import nl.knaw.huc.broccoli.api.ResourcePaths.REPUBLIC
@@ -8,10 +7,7 @@ import nl.knaw.huc.broccoli.config.BroccoliConfiguration
 import nl.knaw.huc.broccoli.config.RepublicConfiguration
 import nl.knaw.huc.broccoli.config.RepublicVolume
 import nl.knaw.huc.broccoli.service.IIIFStore
-import nl.knaw.huc.broccoli.service.ResourceLoader
 import nl.knaw.huc.broccoli.service.anno.AnnoRepo
-import org.eclipse.jetty.util.ajax.JSON
-import org.glassfish.jersey.client.ClientProperties
 import org.slf4j.LoggerFactory
 import java.net.URI
 import javax.ws.rs.*
@@ -34,39 +30,12 @@ class RepublicResource(
 
     private val volumeMapper = VolumeMapper(configuration.republic)
 
-//    private val cache = HashMap<Pair<String, Int>, AnnoTextResult>()
-
-    @GET
-    @Path("v0")
-    @Operation(
-        deprecated = true,
-        description = "Get mock text, annotations and iiif details for given volume, opening and bodyId (opt)"
-    )
-    fun getVolumeOpeningMocked(
-        @QueryParam("volume") _volume: String?,
-        @QueryParam("opening") _opening: Int?,
-        @QueryParam("bodyId") _bodyId: String?
-    ): Response {
-        val volume = _volume ?: configuration.republic.defaultVolume
-        val opening = _opening ?: configuration.republic.defaultOpening
-
-        log.info("volume: $volume, opening: $opening, bodyId: $_bodyId")
-
-        if (_bodyId == null) {
-            return Response.ok(buildResult(volume, opening)).build()
-        }
-
-        val bodyId = _bodyId.removePrefix("urn:example:republic:")
-        val path = "mock/$bodyId.json"
-        log.info("path: $path")
-        val reader = ResourceLoader.asStream(path)
-        val body = jacksonObjectMapper().readValue(reader, AnnoTextBody::class.java)
-        return Response.ok(body).build()
-    }
-
     @GET
     @Path("v1")
-    @Operation(description = "Get text, annotations and iiif details using AnnoRepo and TextRepo")
+    @Operation(
+        deprecated = true,
+        description = "Get text, annotations and iiif details using AnnoRepo and TextRepo"
+    )
     fun getVolumeOpeningFromServers(
         @QueryParam("volume") _volumeId: String?,
         @QueryParam("opening") _openingNo: Int?,
@@ -74,8 +43,8 @@ class RepublicResource(
     ): Response {
         val volumeId = _volumeId ?: configuration.republic.defaultVolume
         val openingNo = _openingNo ?: configuration.republic.defaultOpening
-
         log.info("volumeId: $volumeId, openingNo: $openingNo, bodyId: $_bodyId")
+
         val volume = volumeMapper.byVolumeId(volumeId)
 
         if (_bodyId == null) {
@@ -117,7 +86,6 @@ class RepublicResource(
     ): Response {
         val volumeId = _volumeId ?: configuration.republic.defaultVolume
         val openingNo = _openingNo ?: configuration.republic.defaultOpening
-
         log.info("volumeId: $volumeId, openingNo: $openingNo, bodyId: $_bodyId")
 
         val volume = volumeMapper.byVolumeId(volumeId)
@@ -237,89 +205,6 @@ class RepublicResource(
         log.info("fetching took ${System.currentTimeMillis() - startTime} ms")
         return result
     }
-
-    private fun buildResult(volumeId: String, opening: Int, bodyId: String? = null): AnnoTextResult {
-        if (opening < 1) {
-            throw BadRequestException("Opening count starts at 1 (but got: $opening)")
-        }
-
-        val volume = volumeMapper.byVolumeId(volumeId)
-        log.info("client.timeout (before call): ${client.configuration.getProperty(ClientProperties.READ_TIMEOUT)}")
-
-        val canvasId = iiifStore.getCanvasId(volumeId, opening)
-        val mockedAnnotations = loadMockAnnotations()
-
-        val request = mapOf(
-            "volumeId" to volumeId,
-            "openingNo" to opening.toString()
-        ).toMutableMap()
-
-        if (bodyId != null) request["bodyId"] = bodyId
-
-        return AnnoTextResult(
-            request = request,
-            anno = mockedAnnotations.filter { !setOf("line", "column", "textregion").contains(getBodyValue(it)) },
-            text = getMockedText(mockedAnnotations),
-            iiif = IIIFContext(
-                manifest = manifest(volume),
-                canvasId = canvasId
-            )
-        )
-    }
-
-    private fun getMockedText(annos: List<Map<String, Any>>): List<String> {
-        val anno = annos[0]
-        val id = anno["id"]
-        log.info("anno[0].id = $id")
-
-        val scanpage = annos.find { getBodyValue(it) == "scanpage" }.orEmpty()
-        log.info("scanpage: $scanpage")
-
-        val target = scanpage["target"] as Array<*>
-        val textTarget = target.find { (it as HashMap<*, *>)["type"] == "Text" } as HashMap<*, *>
-        val textSelector = textTarget["selector"] as HashMap<*, *>
-        val start = textSelector["start"]
-        val end = textSelector["end"]
-        log.info("start=$start, end=$end")
-
-        val mockedText = ArrayList<String>()
-
-        val reader = ResourceLoader.asReader("mock/vol_1728-opening_285.json")
-        val json = JSON.parse(reader)
-        log.info("json: ${JSON.toString(json)}")
-        if (json is Array<*>) {
-            for (line in json) {
-                if (line is String) {
-                    mockedText.add(line)
-                }
-            }
-        }
-        return mockedText
-    }
-
-    private fun getBodyValue(anno: Map<String, Any>): String? {
-        val body = anno["body"]
-        return if (body is HashMap<*, *>) body["value"] as String else null
-    }
-
-    private fun loadMockAnnotations(): List<Map<String, Any>> {
-        val mockedAnnos = ArrayList<Map<String, Any>>()
-        for (i in 0..3) {
-            val reader = ResourceLoader.asReader("mock/anno-page-$i.json")
-            val annoPage = JSON.parse(reader)
-            if (annoPage is HashMap<*, *>) {
-                val items = annoPage["items"]
-                if (items is Array<*>) {
-                    @Suppress("UNCHECKED_CAST")
-                    items.forEach { if (it is Map<*, *>) mockedAnnos.add(it as Map<String, Any>) }
-                } else {
-                    log.info("AnnotationPage[\"items\"] not a JSON array, skipping: $items.")
-                }
-            }
-        }
-        return mockedAnnos
-    }
-
 }
 
 class VolumeMapper(private val config: RepublicConfiguration) {
