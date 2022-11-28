b541988636ac9c8d9e5c297fb1fce698ccad7b27
HDJ <hayco@users.noreply.github.com>
2022-11-28 10:22:08 +0100

    Add /globalise endpoint

--- src/main/kotlin/nl/knaw/huc/broccoli/resources/globalise/GlobaliseResource.kt
+++ src/main/kotlin/nl/knaw/huc/broccoli/resources/globalise/GlobaliseResource.kt
@@ -1,4 +1,8 @@
 package nl.knaw.huc.broccoli.resources.globalise
 
-class GlobaliseResource {
+import nl.knaw.huc.broccoli.config.GlobaliseConfiguration
+import org.slf4j.LoggerFactory
+
+class GlobaliseResource(private val globalise: GlobaliseConfiguration) {
+    private val log = LoggerFactory.getLogger(javaClass)
 }
