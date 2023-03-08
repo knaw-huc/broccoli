e091dcd60e0e50e41763f6d48ac57a3c6f732792
HDJ <hayco@users.noreply.github.com>
2023-03-08 10:37:03 +0100

    yield overlapTypes as list, rather than as String

--- src/main/kotlin/nl/knaw/huc/broccoli/resources/projects/ProjectsResource.kt
+++ src/main/kotlin/nl/knaw/huc/broccoli/resources/projects/ProjectsResource.kt
@@ -84,7 +84,7 @@ class ProjectsResource(
                 "projectId" to projectId,
                 "bodyId" to bodyId,
                 "includeResults" to interestedIn,
-                "overlapTypes" to overlapTypesParam,
+                "overlapTypes" to overlapTypes,
                 "relativeTo" to relativeTo
             )
         )
