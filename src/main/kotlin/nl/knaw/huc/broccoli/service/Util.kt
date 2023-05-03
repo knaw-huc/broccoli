177c4d4c2b054a1fa6edf6d2bdeba9379971e920
HDJ <hayco@users.noreply.github.com>
2023-05-03 15:10:42 +0200

    0.17.4: remove empty buckets terms from search result
    This happens when heterogeneous objects are in the same index, and a search is done which only yields objects of one type. Then all the fields which exist only in the other type end up in the aggregations with empty buckets.

--- src/main/kotlin/nl/knaw/huc/broccoli/service/Util.kt
+++ src/main/kotlin/nl/knaw/huc/broccoli/service/Util.kt
@@ -6,14 +6,14 @@ fun <K, V> List<Map<K, V>>.groupByKey(): Map<K, V> = flatMap { it.asSequence() }
 
 // migrate to ES specific 'util'
 fun extractAggregations(context: ReadContext) = context.read<Map<String, Any>>("$.aggregations")
-    ?.map { aggregation ->
+    ?.mapNotNull { aggregation ->
         @Suppress("UNCHECKED_CAST")
-        val buckets = (aggregation.value as Map<String, Any>)["buckets"]
+        val buckets = (aggregation.value as Map<String, Any>)["buckets"] as List<Map<String, Any>>
 
-        @Suppress("UNCHECKED_CAST")
-        val associatedValues = (buckets as List<Map<String, Any>>)
-            .associate { (it["key_as_string"] ?: it["key"]) to it["doc_count"] }
-
-        mapOf(aggregation.key to associatedValues)
+        if (buckets.isEmpty())
+            null
+        else {
+            mapOf(aggregation.key to buckets.associate { (it["key_as_string"] ?: it["key"]) to it["doc_count"] })
+        }
     }
     ?.groupByKey()
