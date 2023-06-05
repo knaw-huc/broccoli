01912002760576d26e0d7154feb23649ee8fcb9e
HDJ <hayco@users.noreply.github.com>
2023-06-05 11:01:34 +0200

    Boost cache capacity

--- src/main/kotlin/nl/knaw/huc/broccoli/service/anno/AnnoRepo.kt
+++ src/main/kotlin/nl/knaw/huc/broccoli/service/anno/AnnoRepo.kt
@@ -149,7 +149,7 @@ class AnnoRepo(
     data class Offset(val value: Int, val id: String)
 
     companion object {
-        const val CACHE_CAPACITY = 100
-        const val CACHE_RESULT_SET_SIZE_THRESHOLD = 100
+        const val CACHE_CAPACITY = 10000
+        const val CACHE_RESULT_SET_SIZE_THRESHOLD = 1000
     }
 }
