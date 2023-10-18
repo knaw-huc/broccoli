09ddc07674b4423f871e23e5e6cd8ff40f312224
HDJ <hayco@users.noreply.github.com>
2023-10-18 13:57:26 +0200

    0.26.2 Expose current 'nl.knaw.huc' log-level in /about

--- src/main/kotlin/nl/knaw/huc/broccoli/resources/AboutResource.kt
+++ src/main/kotlin/nl/knaw/huc/broccoli/resources/AboutResource.kt
@@ -1,5 +1,7 @@
 package nl.knaw.huc.broccoli.resources
 
+import ch.qos.logback.classic.Logger
+import ch.qos.logback.classic.LoggerContext
 import io.swagger.v3.oas.annotations.Operation
 import jakarta.ws.rs.GET
 import jakarta.ws.rs.Path
@@ -8,19 +10,31 @@ import jakarta.ws.rs.core.MediaType
 import nl.knaw.huc.broccoli.api.AboutInfo
 import nl.knaw.huc.broccoli.api.ResourcePaths
 import nl.knaw.huc.broccoli.config.BroccoliConfiguration
+import org.slf4j.LoggerFactory
 import java.time.Instant
 
 @Path(ResourcePaths.ABOUT)
 @Produces(MediaType.APPLICATION_JSON)
-class AboutResource(configuration: BroccoliConfiguration, appName: String, version: String) {
-    private val about = AboutInfo(
-        appName = appName,
-        version = version,
-        startedAt = Instant.now().toString(),
-        baseURI = configuration.externalBaseUrl
-    )
+class AboutResource(
+    private val configuration: BroccoliConfiguration,
+    private val appName: String,
+    private val version: String
+) {
+    private val startedAt = Instant.now().toString()
 
     @Operation(description = "Get basic server information")
     @GET
-    fun getAboutInfo(): AboutInfo = about
+    fun getAboutInfo(): AboutInfo = AboutInfo(
+        appName = appName,
+        version = version,
+        startedAt = startedAt,
+        baseURI = configuration.externalBaseUrl,
+        hucLogLevel = findHucLogLevel()
+    )
+
+    private fun findHucLogLevel(): String {
+        val loggerContext = LoggerFactory.getILoggerFactory()
+        val logger: Logger = (loggerContext as LoggerContext).getLogger("nl.knaw.huc")
+        return logger.level.toString()
+    }
 }
