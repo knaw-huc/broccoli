5de206832ed3e2fae511608a75230ed11fd1e647
Hayco de Jong <hayco.de.jong@di.huc.knaw.nl>
2024-09-02 13:51:48 +0200

    internalise-view-annotation-filtering (#66)
    Pre-fetch all overlapping anno's in bulk, then do local filtering on constraints and inclusion for each view. This saves numerous AnnoRepo calls from previous implementation.

--- src/main/kotlin/nl/knaw/huc/broccoli/service/anno/AnnoRepoSearchResult.kt
+++ src/main/kotlin/nl/knaw/huc/broccoli/service/anno/AnnoRepoSearchResult.kt
@@ -1,10 +1,13 @@
 package nl.knaw.huc.broccoli.service.anno
 
 import com.jayway.jsonpath.DocumentContext
+import nl.knaw.huc.broccoli.service.getValueAtPath
 
 class AnnoRepoSearchResult(val context: DocumentContext) {
+    fun root(): Map<String, Any> = context.read("$")
+
     fun items(): List<Map<String, Any>> {
-        return listOf(context.read("$"))
+        return listOf(root())
     }
 
     fun bodyId(): String = context.read("$.body.id")
@@ -23,4 +26,17 @@ class AnnoRepoSearchResult(val context: DocumentContext) {
 
     fun <T> withoutField(type: String, field: String): List<Map<String, T>> =
         context.read("$.target[?(@.type == '$type')][?(@.$field == null)]")
+
+    fun liesWithin(region: IntRange) =
+        target<Any>("Text")
+            .any { target ->
+                getValueAtPath<Int>(target, "selector.start")?.let { start ->
+                    getValueAtPath<Int>(target, "selector.end")?.let { end ->
+                        start >= region.first && end <= region.last
+                    } ?: false
+                } ?: false
+            }
+
+    fun satisfies(constraints: Map<String, String>) =
+        constraints.all { (path, value) -> read("$.$path") == value }
 }
