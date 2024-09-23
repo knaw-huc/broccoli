34d15af2627c0c4a72e82423c97ccd998e79fe45
HDJ <hayco.de.jong@di.huc.knaw.nl>
2024-09-23 14:45:49 +0200

    Abstract deep nested ES result to bitesize :broccoli:

--- src/main/kotlin/nl/knaw/huc/broccoli/service/Util.kt
+++ src/main/kotlin/nl/knaw/huc/broccoli/service/Util.kt
@@ -6,12 +6,37 @@ import com.jayway.jsonpath.ReadContext
 fun extractAggregations(context: ReadContext) = context.read<Map<String, Any>>("$.aggregations")
     ?.mapNotNull { aggregation ->
         @Suppress("UNCHECKED_CAST")
-        val buckets = (aggregation.value as Map<String, Any>)["buckets"] as List<Map<String, Any>>
-
-        if (buckets.isEmpty())
-            null
-        else {
-            mapOf(aggregation.key to buckets.associate { (it["key_as_string"] ?: it["key"]) to it["doc_count"] })
+        val aggValuesMap = aggregation.value as Map<String, Any>
+        if ("buckets" in aggValuesMap) {
+            @Suppress("UNCHECKED_CAST")
+            val buckets = aggValuesMap["buckets"] as List<Map<String, Any>>
+            if (buckets.isEmpty())
+                null
+            else {
+                mapOf(aggregation.key to buckets.associate { (it["key_as_string"] ?: it["key"]) to it["doc_count"] })
+            }
+        } else {
+            mapOf(
+                aggregation.key to mutableListOf<Map<String, Any>>().apply {
+                    aggValuesMap
+                        .filter { it.key != "doc_count" }
+                        .forEach { (nestedFacetName: String, nestedFacetValues: Any) ->
+                            val nestedAggValuesMap = nestedFacetValues as Map<*, *>
+                            if ("buckets" in nestedAggValuesMap) {
+                                @Suppress("UNCHECKED_CAST")
+                                val nestedBuckets = nestedAggValuesMap["buckets"] as List<Map<String, Any>>
+                                if (nestedBuckets.isNotEmpty()) {
+                                    add(
+                                        mapOf(nestedFacetName to nestedBuckets.associate {
+                                            (it["key_as_string"] ?: it["key"]) to
+                                                    (it["documents"] as Map<*, *>)["doc_count"]
+                                        })
+                                    )
+                                }
+                            }
+                        }
+                }.groupByKey()
+            )
         }
     }
     ?.groupByKey()
