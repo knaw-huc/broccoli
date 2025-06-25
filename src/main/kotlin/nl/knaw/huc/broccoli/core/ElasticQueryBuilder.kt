97887845261188b12f834b48c6e860dac4e71265
Hayco de Jong <hayco.de.jong@di.huc.knaw.nl>
2025-06-25 15:15:07 +0200

    Allow selection of specific views for full text query

--- src/main/kotlin/nl/knaw/huc/broccoli/core/ElasticQueryBuilder.kt
+++ src/main/kotlin/nl/knaw/huc/broccoli/core/ElasticQueryBuilder.kt
@@ -188,25 +188,37 @@ class ElasticQueryBuilder(private val index: IndexConfiguration) {
         }
 
     private fun normalizeQuery(query: IndexQuery): IndexQuery {
+        val available = index.fields.filter { it.type == "text" }.map { it.name }
+
+        val todoViews = query.textViews?.apply {
+            filterNot { it in available }.apply {
+                if (isNotEmpty())
+                    throw BadRequestException("Unknown textView(s): $this, available: $available")
+            }
+        } ?: listOf("text").plus(available)
+
+        logger.atTrace()
+            .addKeyValue("available", available)
+            .addKeyValue("requested", query.textViews)
+            .addKeyValue("todoViews", todoViews)
+            .log("computed views")
+
+        val todoText = query.text?.trim()?.let { q ->
+            if (ES_FIELD_PREFIX.matches(q)) q
+            else {
+                todoViews.joinToString(separator = " OR ") { "${it}:$q" }
+            }
+        }
+
+        logger.atTrace().addKeyValue("text", todoText).log("computed text")
+
         return IndexQuery(
             date = query.date,
             range = query.range,
             terms = query.terms,
             aggregations = query.aggregations,
-            text = query.text
-                ?.trim()
-                ?.let { q ->
-                    if (ES_FIELD_PREFIX.matches(q)) q else buildString {
-                        append("text:$q")
-                        index.fields
-                            .filter { it.type == "text" }
-                            .map { it.name }
-                            .forEach { fieldName ->
-                                append(" OR ")
-                                append("$fieldName:$q")
-                            }
-                    }
-                },
+            textViews = todoViews,
+            text = todoText
         )
     }
 
@@ -313,7 +325,7 @@ class ElasticQueryBuilder(private val index: IndexConfiguration) {
             ?: "unknown"
 
     companion object {
-        private val logger = LoggerFactory.getLogger(ElasticQueryBuilder::class.java)
+        private val logger = LoggerFactory.getLogger(ElasticQueryBuilder::class.java.simpleName)
 
         private val ES_FIELD_PREFIX = """^[a-zA-Z]*:""".toRegex()
 
