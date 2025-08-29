09cce2a429d2aea732f15037a3d4dbcfbef7d91f
Hayco de Jong <hayco.de.jong@di.huc.knaw.nl>
2025-08-29 11:28:11 +0200

    0.48.0-SNAPSHOT: fix concurrent access in LRUCache

--- src/main/kotlin/nl/knaw/huc/broccoli/service/cache/LRUCache.kt
+++ src/main/kotlin/nl/knaw/huc/broccoli/service/cache/LRUCache.kt
@@ -1,29 +1,31 @@
 package nl.knaw.huc.broccoli.service.cache
 
 import org.slf4j.LoggerFactory
-import java.util.*
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.LinkedBlockingDeque
 
 class LRUCache<K, V>(private val capacity: Int = 5) {
     private val log = LoggerFactory.getLogger(javaClass)
 
-    private val cache = HashMap<K, V>()
+    private val cache = ConcurrentHashMap<K & Any, V & Any>()
 
     // LinkedList chosen over ArrayList for O(1) removeFirst and addLast so eviction stays O(1)
-    private val insertionOrder = LinkedList<K>()
+    private val insertionOrder = LinkedBlockingDeque<K & Any>()
 
     // O(1) amortised over load factor get
     fun get(key: K): V? = cache[key]
 
     // O(1) amortised over load factor put and eviction
-    fun put(key: K, value: V): K? {
+    fun put(key: K & Any, value: V & Any): K? {
         var evictedKey: K? = null
         if (cache.size >= capacity) {
             evictedKey = insertionOrder.removeFirst()
             cache.remove(evictedKey)
             log.info("cache miss: evicted [$evictedKey] to make room for [$key]")
         }
-        cache[key] = value
-        insertionOrder.addLast(key)
+        if (insertionOrder.offer(key)) {
+            cache[key] = value
+        }
         return evictedKey
     }
 
